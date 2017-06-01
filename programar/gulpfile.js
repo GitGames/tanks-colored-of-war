5521d54e78007a1a5c588dd1a6e5a9a475fb5bed
ottohernandezgarzon <ohernandezgarzon@gmail.com>
2017-06-01 12:06:18 -0500

    actualizo son de trabajo

--- programar/gulpfile.js
+++ programar/gulpfile.js
@@ -1,19 +1,10 @@
 const gulp = require('gulp'),
     pug = require('gulp-pug'),
-    html = require('gulp-html2pug'),
     jshint = require('gulp-jshint'),
     // gamer = require('phaser-ce'),
     browser = require('browser-sync').create(),
     error = require('gulp-plumber');
-
-// sincronzar las pagina web 
-gulp.task('servidor', ()=> {
-    browser.init({
-        server: {
-            baseDir: "../Game"
-        }
-    });
-});
+// Correccion del control de  tareas
 gulp.task('errorGulp',() =>
     gulp.src('gulpfile.js')
     .pipe(error()) 
@@ -21,17 +12,23 @@ gulp.task('errorGulp',() =>
     .pipe(jshint.reporter('Default'))
     .pipe(error.stop())
 );
+// sincronzar las pagina web 
+gulp.task('servidor', ()=> {
+    browser.init({
+        server: {
+            baseDir: "../Game"
+        }
+    });
 
-// tareas del convertidor pug a html y sentido contrario
-gulp.task('html', ()=> {
-    gulp.src('../Game/*.html')
-        .pipe(error())
-        .pipe(html())
-        .pipe(error.stop())
-        .pipe(browser.reload({ stream: true }))
-        .pipe(gulp.dest('./documento/'));
 });
-gulp.task('pug', ['html'], ()=> {
+gulp.task('js', () => {
+    gulp.src('../game/js/../*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('Default'))
+    .pipe(browser.reload({ stream: true }))
+});
+// tareas del convertidor pug a html y sentido contrario);
+gulp.task('pug',()=> {
     gulp.src('./documento/*.pug')
         .pipe(error())
         .pipe(pug({ pretty: true }))
@@ -41,7 +38,7 @@ gulp.task('pug', ['html'], ()=> {
 });
 
 //tarea de atomatizacion
-gulp.task('default', ['servidor', 'errorGulp', 'html', 'pug'], () => {
+gulp.task('default', ['servidor','js', 'errorGulp', 'html', 'pug'], () => {
     gulp.watch('../Game/*.html', ['html']);
     gulp.watch('./documento/*.pug', ['pug']);
     gulp.watch('./gulpfile.js', ['errorGulp']);
